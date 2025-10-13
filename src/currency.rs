11482d7fcb7d14769af301cc5877aa72a84627cb
fellowseb <wauquier.sebastien@gmail.com>
2025-10-13 16:51:18 +0200

    Chapter 5

--- src/currency.rs
+++ src/currency.rs
@@ -22,9 +22,31 @@ impl fmt::Display for Dollar {
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
+pub struct Franc {
+    amount: u32,
+}
+
+impl Franc {
+    pub fn new(amount: u32) -> Franc {
+        Franc { amount: amount }
+    }
+    pub fn times(&self, multiplier: u32) -> Self {
+        Franc {
+            amount: self.amount * multiplier,
+        }
+    }
+}
+
+impl fmt::Display for Franc {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("${}", self.amount))
+    }
+}
+
 #[cfg(test)]
 mod test {
-    use crate::currency::Dollar;
+    use crate::currency::{Dollar, Franc};
 
     #[test]
     fn test_multiplication() {
@@ -37,5 +59,12 @@ mod test {
     fn test_equality() {
         assert_eq!(Dollar::new(5), Dollar::new(5));
         assert_ne!(Dollar::new(5), Dollar::new(6));
+    }
+
+    #[test]
+    fn test_franc_multiplication() {
+        let five = Franc::new(5);
+        assert_eq!(Franc::new(10), five.times(2));
+        assert_eq!(Franc::new(15), five.times(3));
     }
 }
