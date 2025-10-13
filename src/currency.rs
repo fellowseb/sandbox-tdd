507ddb437340bca9cdc0185f91d1dac0e995f59e
fellowseb <wauquier.sebastien@gmail.com>
2025-10-13 20:55:27 +0200

    Chapters 6,7,8,9,10 & 11

--- src/currency.rs
+++ src/currency.rs
@@ -1,70 +1,57 @@
 use core::fmt;
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct Dollar {
-    amount: u32,
+#[derive(Debug, PartialEq)]
+enum Currency {
+    Dollar,
+    Franc,
 }
 
-impl Dollar {
-    pub fn new(amount: u32) -> Dollar {
-        Dollar { amount: amount }
+#[derive(Debug, PartialEq)]
+pub struct Money(u32, Currency);
+
+impl Money {
+    pub fn dollar(amount: u32) -> Money {
+        Money(amount, Currency::Dollar)
     }
-    pub fn times(&self, multiplier: u32) -> Self {
-        Dollar {
-            amount: self.amount * multiplier,
+    pub fn franc(amount: u32) -> Money {
+        Money(amount, Currency::Franc)
+    }
+    pub fn times(&self, multiplier: u32) -> Money {
+        let product = self.0 * multiplier;
+        match self.1 {
+            Currency::Franc => Money(product, Currency::Franc),
+            Currency::Dollar => Money(product, Currency::Dollar),
         }
     }
 }
 
-impl fmt::Display for Dollar {
+impl fmt::Display for Money {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!("${}", self.amount))
-    }
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub struct Franc {
-    amount: u32,
-}
-
-impl Franc {
-    pub fn new(amount: u32) -> Franc {
-        Franc { amount: amount }
-    }
-    pub fn times(&self, multiplier: u32) -> Self {
-        Franc {
-            amount: self.amount * multiplier,
-        }
-    }
-}
-
-impl fmt::Display for Franc {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!("${}", self.amount))
+        let currency_str = match self.1 {
+            Currency::Franc => "CHF",
+            Currency::Dollar => "$",
+        };
+        f.write_fmt(format_args!("{}{}", currency_str, self.0))
     }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::currency::{Dollar, Franc};
+    use crate::currency::Money;
 
     #[test]
     fn test_multiplication() {
-        let five = Dollar::new(5);
-        assert_eq!(Dollar::new(10), five.times(2));
-        assert_eq!(Dollar::new(15), five.times(3));
+        let five = Money::dollar(5);
+        assert_eq!(Money::dollar(10), five.times(2));
+        assert_eq!(Money::dollar(15), five.times(3));
     }
 
     #[test]
     fn test_equality() {
-        assert_eq!(Dollar::new(5), Dollar::new(5));
-        assert_ne!(Dollar::new(5), Dollar::new(6));
-    }
-
-    #[test]
-    fn test_franc_multiplication() {
-        let five = Franc::new(5);
-        assert_eq!(Franc::new(10), five.times(2));
-        assert_eq!(Franc::new(15), five.times(3));
+        assert_eq!(Money::dollar(5), Money::dollar(5));
+        assert_ne!(Money::dollar(5), Money::dollar(6));
+        assert_eq!(Money::franc(5), Money::franc(5));
+        assert_ne!(Money::franc(5), Money::franc(6));
+        assert_ne!(Money::franc(5), Money::dollar(5));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use sandbox_tdd::currency::Dollar;
+use sandbox_tdd::currency::Money;
 
 fn main() {
-    let d = Dollar::new(5);
+    let d = Money::dollar(5);
     const T: u32 = 2;
     let product = d.times(T);
     println!("{} times {} is {}", d, T, product);
